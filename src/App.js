8626fb39d9beb9759cdd40b53d432cb292d05547
Agustin Espana <agustinespania@gmail.com>
2021-11-26 12:24:10 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
       .then(initialPersons => {
         setPersons(initialPersons)
       });
-  }, [])
+  }, []);
 
   const addPerson = (event) => {
 		event.preventDefault();
